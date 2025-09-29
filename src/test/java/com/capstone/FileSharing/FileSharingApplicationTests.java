6f2af796c3e80e9112fc2fe77cad4e7ccefb49c3
jonnadula sashank <jonnadulasashank@amb20074347l.stryker.cn>
2025-09-29 22:22:13 +0530

    INIT

---
+++ src/test/java/com/capstone/FileSharing/FileSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.FileSharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
